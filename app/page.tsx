5abe8c04414c5f163648742fb2f7d007557472e5
zeNOPE <nobel2545@gmail.com>
2024-04-09 02:50:34 +0700

    ใส่แฟ้มจ้า

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function Home() {
   return (
-    
-    <div>
-      <img className=" max-w-[1920px] max-h-[1080px] static bg-cover" src="roomBackground.jpg" />
 
-      <img className=" absolute ml-10 p-10" src="folder_gacha.svg" />
-      <img className=" w-[400px] h-[400px] absolute top-[237px] left-[650px]" src="sohm.gif"/>
+    <div className="" >
+      <img className=" max-w-[1920px] max-h-[750px] fixed bg-cover" src="roomBackground.jpg" />
+
+      <img className=" w-[400px] absolute ml-10 p-10" src="folder_gacha.svg" />
+      <img className=" w-[350px] h-[350px] absolute top-[105px] left-[415px]" src="sohm.gif"/>
+
+      <Swiper className="" spaceBetween={5} slidesPerView={2.5}>
+        <SwiperSlide className="tag-menu">Slide 1</SwiperSlide>
+        <SwiperSlide className="tag-menu">Slide 2</SwiperSlide>
+        <SwiperSlide className="tag-menu">Slide 3</SwiperSlide>
+        <SwiperSlide className="tag-menu">Slide 4</SwiperSlide>
+      </Swiper>
     
     </div>
   );
