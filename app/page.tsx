df85a3a51cca6ec2ae99c2a699de9eb19fc5a43e
zeNOPE <nobel2545@gmail.com>
2024-04-09 03:05:00 +0700

    อีกสักที

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
         <img className=" max-w-[1920px] max-h-[750px] fixed bg-cover" src="roomBackground.jpg" />
 
         <img className=" w-[400px] absolute ml-10 p-10" src="folder_gacha.svg" />
-        <img className=" w-[350px] h-[350px] absolute top-[105px] left-[415px]" src="sohm.gif"/>
+        <img className=" w-[350px] h-[350px] absolute top-[140px] left-[415px]" src="sohm.gif"/>
       </div>
 
-      <Swiper className="" spaceBetween={5} slidesPerView={2.5}>
+      <Swiper className="" spaceBetween={50} slidesPerView={2.5}>
         <SwiperSlide className="tag-menu">Slide 1</SwiperSlide>
         <SwiperSlide className="tag-menu">Slide 2</SwiperSlide>
         <SwiperSlide className="tag-menu">Slide 3</SwiperSlide>
